147d4be2a18e84f5629c4079dfd498ccb0571ce8
Tkachenko_Yuriy <uvt86@ukr.net>
2022-10-23 14:13:37 +0300

    add vimeo_player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "videoplayer-current-time"
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(STORAGE_KEY, seconds);
+  }, 1000)
+);
+
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(STORAGE_KEY) || 0;
+  player.setCurrentTime(currentTime);
+});
